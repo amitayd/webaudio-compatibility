7bf0cd00ea3854c04337805024c4f5eb0f2cd9fb
Amitay Dobo <amitayd@gmail.com>
2013-10-22 21:11:16 +0300

    changed browser scope results to show major versions by default

--- js/app.js
+++ js/app.js
@@ -32,13 +32,13 @@ function main(settings) {
     var newScript = document.createElement('script'),
       container = document.getElementById(settings.browserScopeContainerId);
     newScript.src = 'http://www.browserscope.org/user/tests/table/' + 
-        settings.browserScopeKey + '?o=js&v=3&f=AudioContext,AudioContext.correctName,AudioContext.createOscillator&highlight=1';
+        settings.browserScopeKey + '?o=js&v=1&f=AudioContext,AudioContext.correctName,AudioContext.createOscillator&highlight=1';
     container.appendChild(newScript);
   }
 
   if (settings.browserScopeLinkId) {
     var linkEl = document.getElementById(settings.browserScopeLinkId);
-    linkEl.href = 'http://www.browserscope.org/user/tests/table/' + settings.browserScopeKey + '?v=3&highlight=1&layout=simple';
+    linkEl.href = 'http://www.browserscope.org/user/tests/table/' + settings.browserScopeKey + '?v=1&highlight=1&layout=simple';
   }
 
 
